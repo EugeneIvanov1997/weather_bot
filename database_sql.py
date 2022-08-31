bd4423a54111d19113570cf7735c35dda954230d
EugeneIvanov1997 <76510956+EugeneIvanov1997@users.noreply.github.com>
2022-08-31 17:39:31 +0700

    Update database_sql.py

--- database_sql.py
+++ database_sql.py
@@ -2,6 +2,11 @@ import sqlite3 as sq
 
 
 def sql_start():
+    """
+    Создаем БД, если ее еще нет, и подключаемся к ней
+    Также создаем таблицу для пользователей с их id, языком и единицами измерения
+    """
+    
     global base, cur
     base = sq.connect('users.db')
     cur = base.cursor()
@@ -12,6 +17,10 @@ def sql_start():
 
 
 async def add_user(user_id: str, user_lang: str, user_units: str):
+    """
+    Добавление нового пользователя в БД
+    """
+    
     try:
         cur.execute('INSERT INTO users_data VALUES (?, ?, ?)', (user_id, user_lang, user_units))
         base.commit()
@@ -20,16 +29,28 @@ async def add_user(user_id: str, user_lang: str, user_units: str):
 
 
 async def edit_language(user_id: str, user_lang: str):
+    """
+    Изменение языка пользователя
+    """
+    
     cur.execute('UPDATE users_data SET user_language == ? WHERE user_id == ?', (user_lang, user_id))
     base.commit()
 
 
 async def edit_units(user_id: str, user_units: str):
+    """
+    Изменение единиц измерения пользователя
+    """
+    
     cur.execute('UPDATE users_data SET user_units == ? WHERE user_id == ?', (user_units, user_id))
     base.commit()
 
 
 async def get_user_language(user_id: str):
+    """
+    Функция для получения языка пользователя по id
+    """
+    
     try:
         lang = cur.execute('SELECT user_language FROM users_data WHERE user_id == ?', (user_id,)).fetchone()[0]
     except:
