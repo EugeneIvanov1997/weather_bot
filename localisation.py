46bd82030e6b4e3fb7b7d997ff9bd092e4b43edc
EugeneIvanov1997 <76510956+EugeneIvanov1997@users.noreply.github.com>
2022-08-31 17:36:32 +0700

    Update localisation.py

--- localisation.py
+++ localisation.py
@@ -5,11 +5,11 @@ from config import I18N_DOMAIN, LOCALES_DIR
 import database_sql as db
 
 
-class ACLMiddleware(I18nMiddleware):
+class ACLMiddleware(I18nMiddleware): # Создаем класс для middlware
     async def get_user_locale(self, action: str, args: Tuple[Any]) -> Optional[str]:
         user = types.User.get_current()
         lang = await db.get_user_language(str(user.id))
-        return lang if lang is not None else user.locale
+        return lang if lang is not None else user.locale # Перезаписываем функцию для определения языка перевода. Язык будет браться из БД, если он там есть
 
 
 def setup_middleware(dp):
