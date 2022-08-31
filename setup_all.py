ef36ce00e5c44cc9572cba193214914bb23d848c
EugeneIvanov1997 <76510956+EugeneIvanov1997@users.noreply.github.com>
2022-08-31 17:35:10 +0700

    Update setup_all.py

--- setup_all.py
+++ setup_all.py
@@ -4,8 +4,8 @@ from aiogram.dispatcher import Dispatcher
 from localisation import setup_middleware
 
 
-bot = Bot(token=bot_token, parse_mode=types.ParseMode.HTML)
-dp = Dispatcher(bot)
+bot = Bot(token=bot_token, parse_mode=types.ParseMode.HTML) # Создаем объект бота
+dp = Dispatcher(bot) # Создаем диспатчер
 
 i18n = setup_middleware(dp)
-_ = i18n.gettext
+_ = i18n.gettext # Создаем метод для перевода строк
