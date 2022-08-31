1aeda27dd2e2d07d1ea457340abbb778322cd172
EugeneIvanov1997 <76510956+EugeneIvanov1997@users.noreply.github.com>
2022-08-31 16:37:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-open_weather_token = 'YOUR OPENWEATHER API TOKEN HERE'# можно получить тут: https://home.openweathermap.org/api_keys
+open_weather_token = 'YOUR OPENWEATHER API TOKEN HERE' # можно получить тут: https://home.openweathermap.org/api_keys
 bot_token = 'YOUR TELEGRAM BOT TOKEN HERE' # токен бота можно получить в BotFather: https://t.me/BotFather
 
 I18N_DOMAIN = 'weather_bot'
